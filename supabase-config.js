002bae0194b83b0b652286df8ed7d28a810f391c
farelsuraprakarsa-bit <farelsuraprakarsa@gmail.com>
2026-04-23 14:08:10 +0700

    Add Supabase configuration file

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+const supabaseUrl = 'https://ikykcddzywuamztsggrq.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlreWtjZGR6eXd1YW16dHNxZ3JxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NDUxNDAsImV4cCI6MjA5MjMyMTE0MH0.WvY1Jl6Ga5iZYatZpdlRspLjLf4V7BzQ5WK8OCWdnCo'; // 
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
